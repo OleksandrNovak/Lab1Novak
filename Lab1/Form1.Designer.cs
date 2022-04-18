153b139a1f9ceaea2cb5d659a9a8e4b017f0b9b4
hampachovsky <hampe1324@gmail.com>
2022-04-18 17:52:20 +0300

    lab1

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.runButton = new System.Windows.Forms.Button();
+            this.itemTextBox = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.arrayListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(401, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Item";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Array";
+            // 
+            // runButton
+            // 
+            this.runButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.runButton.Location = new System.Drawing.Point(405, 370);
+            this.runButton.Margin = new System.Windows.Forms.Padding(4);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(175, 45);
+            this.runButton.TabIndex = 11;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // itemTextBox
+            // 
+            this.itemTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.itemTextBox.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.itemTextBox.Location = new System.Drawing.Point(405, 59);
+            this.itemTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.itemTextBox.Name = "itemTextBox";
+            this.itemTextBox.Size = new System.Drawing.Size(174, 23);
+            this.itemTextBox.TabIndex = 10;
+            this.itemTextBox.Text = "Тест";
+            this.itemTextBox.WordWrap = false;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.clearButton.Location = new System.Drawing.Point(405, 258);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(175, 45);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.addButton.Location = new System.Drawing.Point(405, 158);
+            this.addButton.Margin = new System.Windows.Forms.Padding(4);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(175, 40);
+            this.addButton.TabIndex = 8;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // arrayListBox
+            // 
+            this.arrayListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.arrayListBox.FormattingEnabled = true;
+            this.arrayListBox.ItemHeight = 17;
+            this.arrayListBox.Items.AddRange(new object[] {"Привіт", "сажотрус", "якісно", "чисто", "саміський"});
+            this.arrayListBox.Location = new System.Drawing.Point(13, 59);
+            this.arrayListBox.Margin = new System.Windows.Forms.Padding(4);
+            this.arrayListBox.Name = "arrayListBox";
+            this.arrayListBox.Size = new System.Drawing.Size(306, 361);
+            this.arrayListBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.itemTextBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.arrayListBox);
+            this.Name = "Form1";
+            this.Text = "Lab1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox itemTextBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.ListBox arrayListBox;
+
+        #endregion
+    }
+}
---
+++ Lab1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        private char charToFind = 'с';
+        private int maxCount = 2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            arrayListBox.Items.Clear();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            arrayListBox.Items.Add(itemTextBox.Text);
+        }
+
+        /// <summary>
+        /// Search array items which are less than at least 2 previous array elements.
+        /// </summary>
+        /// <param name="string">Input string array.</param>
+        /// <returns>The list of sought numbers.</returns>
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < arrayListBox.Items.Count; i++)
+            {
+                int count = 0;
+                
+                if (arrayListBox.Items[i].ToString().ToLower().Contains(charToFind))
+                {
+                    foreach (char c in arrayListBox.Items[i].ToString().ToLower())
+                    {
+                        if (c == charToFind) count++;
+                    }
+                    if (count < maxCount) {
+                        var resultStr = string.Join("\n", arrayListBox.Items[i]);
+                        MessageBox.Show(resultStr);
+                    }
+                }
+
+            }
+
+        }
+
+    }
+}
